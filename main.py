4715bbcc6d28828acfedc7238d7c8f816b2104d4
Amer Ahmad <pixelatedfigure@gmail.com>
2023-07-28 19:29:51 +0300

    Broken down implementation

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import os
 import shutil
 
-dir = "files/"
+def main():
+    files = listFiles("files/")
+    groups = groupBy(files, getPrefix)
 
-for file in os.listdir(dir):
-    lang = file.split('-', 1)[0]
+    for prefix, files in groups.items():
+        targetDirectory = makeDirectory(prefix)
+        moveFiles(files, targetDirectory)
 
-    to = dir + lang
-    
-    if not os.path.exists(to):
-        os.makedirs(to)
+def listFiles(directory):
+    return [os.path.join(directory, file) for file in os.listdir(directory)]
 
-    if os.path.exists(to):
-        file_path = dir + file
-        shutil.move(file_path, to)
+def makeDirectory(name):
+    directory = "files/" + name
+    os.makedirs(directory)
+    return directory
+
+def moveFiles(files, targetDirectory):
+    for file in files:
+        shutil.move(file, targetDirectory)
+
+def getPrefix(filename):
+    return filename.split('-', 1)[0]
+
+def groupBy(items, getKey):
+    result = {}
+    for item in items:
+        key = getKey(item)
+        if key not in result:
+            result[key] = []
+        result[key].append(item)
+    return result
+
+main()
